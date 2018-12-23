eb6321149f864b40bdb917cb3bd0497c06ae5f5c
fandx <haczfdx@163.com>
2018-12-23 10:24:56 +0800

    管理员后台页面登录制作完成

--- info/moduels/admin/__init__.py
+++ info/moduels/admin/__init__.py
@@ -1,6 +1,21 @@
 """这个蓝图对应的是首页相关的操作"""
-from flask import Blueprint
+from flask import Blueprint, session, request, url_for, redirect
 
 admin_blue = Blueprint('admin', __name__)
 
 import info.moduels.admin.views
+
+
+@admin_blue.before_request
+def check_admin():
+    """
+    在登录admin相关的目录请求之前判断session，进行跳转
+    1. 必须是管理员
+    2. 只要不是LOGIN页面
+    :return:
+    """
+    is_admin = session.get('is_admin', False)
+    if not is_admin and not request.url.endswith(url_for("admin.login")):
+        return redirect(url_for("index.index"))
+
+
--- info/moduels/admin/views.py
+++ info/moduels/admin/views.py
@@ -1,29 +1,56 @@
-from flask import render_template, request, jsonify, redirect, url_for
+from _curses import flash
 
+from flask import render_template, request, jsonify, redirect, url_for, current_app, session
+
+from info.models import User
 from info.moduels.admin import admin_blue
 from info.utils.response_code import RET
 
 
 @admin_blue.route("index")
-def admin_index():
+def index():
     return render_template("admin/index.html")
 
 
 @admin_blue.route("/login", methods=["post", "get"])
-def admin_login():
+def login():
     """
     后台页面的制作
     :return:
     """
     if request.method == "GET":
+        is_admin = session.get("is_admin", False)
+        user_id = session.get("user_id", None)
+        if is_admin and user_id:
+            return redirect(url_for("admin.index"))
         return render_template("admin/login.html")
 
     # 获取参数
     username = request.form.get("username")
     password = request.form.get("password")
 
-    # 校验参数
+    # 判断参数
     if not all([username, password]):
-        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+        return render_template('admin/login.html', errmsg="参数错误")
+
+    # 查询当前用户是否是管理员
+    try:
+        user = User.query.filter(User.mobile == username, User.is_admin == True).first()
+    except Exception as e:
+        current_app.logger.error(e)
+        return render_template('admin/login.html', errmsg="数据库出错")
+
+    if not user:
+        return render_template('admin/login.html', errmsg="未查询到用户信息")
+
+    # 校验密码
+    if not user.check_passoword(password):
+        return render_template('admin/login.html', errmsg="用户名或者密码错误")
+
+    # 保存用户的登录信息
+    session["user_id"] = user.id
+    session["mobile"] = user.mobile
+    session["nick_name"] = user.nick_name
+    session["is_admin"] = user.is_admin
 
     return redirect(url_for("admin.index"))
